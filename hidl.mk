1407387405fb03942d0ffc193c1b8cb65ea3f967
dkati <dkatikaridis@gmail.com>
2018-09-29 23:40:02 +0300

    Revert "Bring back our ril and init"
    This reverts commit cab31b35560efbb07c37a6a6ceae8afa0c3ce702.

--- hidl.mk
+++ hidl.mk
@@ -79,8 +79,8 @@ PRODUCT_PACKAGES += \
 
 # Radio
 #PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio.deprecated@1.0
+#    android.hardware.radio@1.0 \
+#    android.hardware.radio.deprecated@1.0
 
 # Sensors HIDL
 PRODUCT_PACKAGES += \
