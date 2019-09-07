a0be551d98569459a4fd080a1c30472cfee00e0d
dkati <dkatikaridis@gmail.com>
2019-09-07 19:22:13 +0300

    bringup.sh: Update

--- bringup.sh
+++ bringup.sh
@@ -1,1 +1,14 @@
-vendor/lineage/build/tools/repopick.py -t ten-build-bringup
+cd build/soong 
+git fetch "https://github.com/LineageOS/android_build_soong" refs/changes/30/255730/7 && git checkout FETCH_HEAD
+cd ../..
+
+cd vendor/lineage
+git fetch "https://github.com/LineageOS/android_vendor_lineage" refs/changes/88/255888/3 && git checkout FETCH_HEAD
+cd ../..
+
+cd build/make
+git fetch "https://github.com/LineageOS/android_build" refs/changes/61/255761/8 && git checkout FETCH_HEAD
+cd ../..
+
+
+#vendor/lineage/build/tools/repopick.py -t ten-build-bringup
